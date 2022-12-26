a37800aadad2022865678af0e9c5cc81b3fdc8ed
Tamagoyaki-1210 <211104@st.yoshida-g.ac.jp>
2022-12-26 13:47:05 +0900

    ブロック配置

--- C++2DBasis001/CLevel.cpp
+++ C++2DBasis001/CLevel.cpp
@@ -1,8 +1,8 @@
 #include "CLevel.h"
 #include "cstdio"
+#include "block.h"
 
-
-CLevel::CLevel(int BlockHeight, int BlockWidth)
+CLevel::CLevel(float BlockHeight, float BlockWidth)
     :m_BlockHeight(BlockHeight), m_BlockWidth(BlockWidth)
 {
 }
@@ -48,7 +48,7 @@ bool CLevel::LoadMapFile(const char* file_path)
             {
             //ブロックの設置	
             case 0:
-
+				CBlock::Create(D3DXVECTOR3(m_BlockWidth+m_BlockWidth * nCntMapW,m_BlockHeight+m_BlockHeight* nCntMapH,0.f), D3DXVECTOR2(m_BlockWidth*2, m_BlockHeight*2));
                 break;
 
             case 1:
